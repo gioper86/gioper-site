0283d5285d1be011f2550ce2e78d78d06e6ab4f9
Giovanni Perna <gioper86@gmail.com>
2020-03-29 16:22:40 +0200

    Tweak layout of the list of articles

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -3,29 +3,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import RoomIcon from '@material-ui/icons/Room';
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import { Link } from "react-router-dom";
 import { posts } from '../../services/dataService';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontSize: 14,
+  }
+});
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
     marginTop: '20px',
   },
-
   cardLinks: {
     color: 'inherit',
     textDecoration: 'none',
+  },
+  sectionHeader: {
+    marginBottom: '20px',
   }
 });
   
@@ -33,50 +37,34 @@ const Articles = () => {
   const classes = useStyles();
   const listPosts = posts.map((post) =>
     <Grid item md={6} xs={12} key={post.id}>
-      <Card className={classes.card}>
+
+      <Card className={classes.root}>
         <Link to={`/ocean/${post.id}`} className={classes.cardLinks}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt=""
-              height="140"
-              image={post.thumbnail}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {post.title}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {post.intro}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
+          <CardHeader
+            title={post.title}
+            subheader={post.publishingDate}
+          />
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/paella.jpg"
+            title="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">{post.intro}</Typography>
+          </CardContent>
         </Link>
-        <CardActions>
-          <List component="nav" aria-label="main mailbox folders">
-            <ListItem button>
-              <ListItemIcon>
-                <RoomIcon />
-              </ListItemIcon>
-              <ListItemText primary={post.location.name} />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <CalendarTodayIcon />
-              </ListItemIcon>
-              <ListItemText primary={post.date} />
-            </ListItem>
-          </List>
-        </CardActions>
       </Card>
     </Grid>
   );
 
   return (
     <Container maxWidth="md" className={classes.root}>
-      <Grid container spacing={3}>
-        {listPosts}
-      </Grid>
+      <div><Typography component="h5" variant="h5" align='center' className={classes.sectionHeader}>Latest posts</Typography></div> 
+      <ThemeProvider theme={theme}>
+        <Grid container spacing={3}>
+            {listPosts}
+        </Grid>
+      </ThemeProvider>
     </Container>
   );
 }
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -6,7 +6,7 @@ export const posts = [
       name: "Sa Tuna, Costa Brava, Spain",
       coordinates: [41.960583, 3.229857]
     },
-    intro: "While freediving in Costa Brava I came across some beautiful soft corals that can be found in the western Mediterranean: Eunicella singularis, known as white gorgonian.",
+    intro: "While freediving in Costa Brava I came across some beautiful soft corals that grow in the western Mediterranean: Eunicella singularis, known as white gorgonian.",
     date: "15/10/2019",
     publishingDate: "02/01/2020",
     thumbnail: "url-to-thumbnail",
@@ -27,6 +27,60 @@ export const posts = [
     ]
   },
   {
+    id: "vrcFhczN",
+    title: "Looking for Pinna Nobilis",
+    location: {
+      name: "Marinella, Palmi, Italy",
+      coordinates: [38.351650, 15.835617]
+    },
+    intro: "some intro in **markdown** here",
+    date: "20/08/2019",
+    publishingDate: "03/01/2020",
+    thumbnail: "url-to-thumbnail",
+    content: [
+      {
+        type: "markdown",
+        parameters: {
+          text: "Lorem ipsum **dolor** sit amet, **consectetur** adipisicing elit. \n\nQuos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.",
+        }, 
+      },
+      {
+        type: "video",
+        parameters: {
+          url: "https://www.youtube.com/embed/QlAJSdJIBqU",
+          description: "some description",
+        }
+      },      
+    ]
+  },
+    {
+    id: "vrcFhczN",
+    title: "Looking for Pinna Nobilis",
+    location: {
+      name: "Marinella, Palmi, Italy",
+      coordinates: [38.351650, 15.835617]
+    },
+    intro: "some intro in **markdown** here",
+    date: "20/08/2019",
+    publishingDate: "03/01/2020",
+    thumbnail: "url-to-thumbnail",
+    content: [
+      {
+        type: "markdown",
+        parameters: {
+          text: "Lorem ipsum **dolor** sit amet, **consectetur** adipisicing elit. \n\nQuos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.",
+        }, 
+      },
+      {
+        type: "video",
+        parameters: {
+          url: "https://www.youtube.com/embed/QlAJSdJIBqU",
+          description: "some description",
+        }
+      },      
+    ]
+  },
+    {
     id: "vrcFhczN",
     title: "Looking for Pinna Nobilis",
     location: {
