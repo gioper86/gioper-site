36f650fe17cb2a90fefc004baf7dc15e26d3107f
Giovanni Perna <gioper86@gmail.com>
2020-03-29 18:26:12 +0200

    Introduce simple express back-end

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const path = require('path');
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
